13c3ee8bd6fecaaa701c65458b939416b2a3c1c2
16gsanket <16gsanket@gmail.com>
2024-02-24 10:22:57 +0530

    minor changes done

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,21 @@
-const mongoose = require('mongoose')
-const plm = require('passport-local-mongoose')
+const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/bankingtest').then(res=>{
-  console.log("connected...")
-}).catch(err=>{
-  console.log("not connected...")
-})
+mongoose
+  .connect("mongodb://127.0.0.1:27017/bankingtest")
+  .then((res) => {
+    console.log("connected...");
+  })
+  .catch((err) => {
+    console.log("not connected...");
+  });
 
 var userSchema = mongoose.Schema({
-  username:String,
-  name:String,
-  password:String
-})
+  username: String,
+  name: String,
+  password: String,
+});
 
-mongoose.plugin(plm)
+mongoose.plugin(plm);
 
-module.exports = mongoose.model("user",userSchema)
-
+module.exports = mongoose.model("user", userSchema);
